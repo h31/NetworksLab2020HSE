6dc3a3e63d8054f14d3217dcf66f6950b69c9f57
Igor Smirnov <piter3343@gmail.com>
2020-04-26 19:02:21 +0300

    rewrite server using poll

--- tcp/server/main.cpp
+++ tcp/server/main.cpp
@@ -9,12 +9,14 @@
 #include <utility>
 #include <vector>
 #include <queue>
-#include <shared_mutex>
 #include <mutex>
 #include <thread>
 #include <algorithm>
 #include <atomic>
 #include <csignal>
+#include <fcntl.h>
+#include <poll.h>
+#include <cassert>
 
 class ClientDisconnectedException : public std::exception {};
 
@@ -33,11 +35,9 @@ class ClientConnection;
 
 class Server {
 public:
-    std::shared_mutex chatHistoryMutex;
     static Server buildServer(uint16_t portno, uint32_t clientsN);
     void run();
     void receiveMessage(const Message& message);
-    void notifyClientIsDead(uint32_t clientId);
     uint32_t chatSize();
     void shutdown();
     const Message& messageAt(uint32_t index);
@@ -46,103 +46,99 @@ public:
 private:
     std::atomic_bool isFinished = false;
     const int sockfd;
-    std::shared_mutex clientsMutex;
     std::vector<ClientConnection *> clients;
-    std::vector<std::thread *> threads;
     std::vector<Message> chatHistory;
-    std::condition_variable_any chatHistoryUpdated;
 
     explicit Server(int sockfd);
 
-    void messageDealing();
-    void notifyClientIsDeadUnsafe(uint32_t clientId);
+    void accept();
+    void deleteDisconnectedClients(std::vector<uint32_t> disconnectedClients);
 };
 
 class ClientConnection {
 public:
     const std::atomic_uint32_t clientId;
+    const int sockfd;
 
     explicit ClientConnection(int sockfd, uint32_t clientId, Server * server);
-    void sendRoutine();
     void run();
-    std::condition_variable_any shouldSendMessage;
+    [[nodiscard]] bool haveToSendMessage() const;
+    void readSomething();
+    void writeSomething();
     void shutdown();
+    void addNewMessages();
+    void tryReadMessages();
     ~ClientConnection();
 
 private:
-    int sockfd;
+    enum MessageState {
+        INTRODUCE_MESSAGE,
+        ROUTINE
+    };
+
+    MessageState state = INTRODUCE_MESSAGE;
     Server * server;
     std::string userName;
-    std::atomic_bool isFinished = false;
     uint32_t nextSend = 0;
+    std::deque<char> sendQueue;
 
-    std::queue<char> untreatedInput;
+
+    std::deque<char> untreatedInput;
     std::string getName();
     Message readMessage();
     std::string readString();
     uint32_t readUInt32();
-    void readSomething();
-    void sendMessage(const Message& message) const;
+    [[nodiscard]] bool canReadNextMessage() const;
+    void sendMessage(const Message& message);
 };
 
 void Message::setTime(uint16_t newTime) {
     time = newTime;
 }
 
-void ClientConnection::shutdown() {
-    isFinished = true;
-    if (::shutdown(sockfd, SHUT_WR) == 0) {
-    }
-    shouldSendMessage.notify_one();
+bool ClientConnection::haveToSendMessage() const {
+    return !sendQueue.empty();
 }
 
-void ClientConnection::sendRoutine() {
-    std::shared_lock chatLock(server->chatHistoryMutex);
-    while (true) {
-        while (server->chatSize() == nextSend && !isFinished) {
-            shouldSendMessage.wait(chatLock);
+void ClientConnection::writeSomething() {
+    static const uint32_t BUFFER_SIZE = 256;
+
+    char buffer[BUFFER_SIZE];
+    int send;
+
+    do {
+        uint32_t bufferPointer = 0;
+        while (bufferPointer < std::min(BUFFER_SIZE, (uint32_t) sendQueue.size())) {
+            buffer[bufferPointer] = sendQueue[bufferPointer];
+            ++bufferPointer;
         }
-        if (isFinished) {
-            break;
+
+        send = write(sockfd, buffer, bufferPointer);
+
+        if (send < 0) {
+            throw ClientDisconnectedException();
         }
-        while (nextSend < server->chatSize()) {
-            try {
-                sendMessage(server->messageAt(nextSend));
-            } catch (ClientDisconnectedException &e) {
-                return;
-            }
-            ++nextSend;
+
+        for (int i = 0; i < send; ++i) {
+            sendQueue.pop_front();
         }
+
+    } while (send != 0 && haveToSendMessage());
+}
+
+void ClientConnection::shutdown() {
+    if (::shutdown(sockfd, SHUT_WR) == 0) {
     }
 }
 
 ClientConnection::~ClientConnection() {
+    std::cerr << "Disconnected user " << clientId << std::endl;
     close(sockfd);
 }
 
 void ClientConnection::run() {
-    try {
-        userName = getName();
-    } catch (ClientDisconnectedException &e) {
-        shutdown();
-
-        server->notifyClientIsDead(clientId);
-        return;
-    }
-
-    {
-        std::thread sendThread([&] { sendRoutine(); });
-        while (!isFinished) {
-            try {
-                Message message = readMessage();
-                server->receiveMessage(message);
-            } catch (ClientDisconnectedException &e) {
-                shutdown();
-            }
-        }
-        sendThread.join();
-    }
-    server->notifyClientIsDead(clientId);
+    std::cerr << "Connected user " << clientId << std::endl;
+    addNewMessages();
 }
 
 std::string ClientConnection::getName() {
@@ -156,26 +152,22 @@ Message ClientConnection::readMessage() {
 
 std::string ClientConnection::readString() {
     uint32_t length = readUInt32();
-    while (untreatedInput.size() < length) {
-        readSomething();
-    }
+    assert(untreatedInput.size() >= length);
     std::string result;
     for (uint32_t i = 0; i < length; ++i) {
         result += untreatedInput.front();
-        untreatedInput.pop();
+        untreatedInput.pop_front();
     }
 
     return result;
 }
 
 uint32_t ClientConnection::readUInt32() {
-    while (untreatedInput.size() < sizeof(uint32_t)) {
-        readSomething();
-    }
+    assert(untreatedInput.size() >= sizeof(uint32_t));
     char buffer[sizeof(uint32_t)];
     for (char& c : buffer) {
         c = untreatedInput.front();
-        untreatedInput.pop();
+        untreatedInput.pop_front();
     }
     uint32_t result;
     memcpy(&result, buffer, sizeof(uint32_t));
@@ -185,13 +177,16 @@ uint32_t ClientConnection::readUInt32() {
 void ClientConnection::readSomething() {
     static const uint32_t BUFFER_SIZE = 256;
     char buffer[BUFFER_SIZE];
-    int received = read(sockfd, buffer, BUFFER_SIZE - 1);
-    if (received <= 0) {
-        throw ClientDisconnectedException();
-    }
-    for (int i = 0; i < received; ++i) {
-        untreatedInput.push(buffer[i]);
-    }
+    int received;
+    do {
+        received = read(sockfd, buffer, BUFFER_SIZE - 1);
+        if (received == 0) {
+            throw ClientDisconnectedException();
+        }
+        for (int i = 0; i < received; ++i) {
+            untreatedInput.push_back(buffer[i]);
+        }
+    } while (received != -1);
 }
 
 Message::Message(std::string senderName, std::string message, uint16_t time) : time(time),
@@ -199,7 +194,7 @@ Message::Message(std::string senderName, std::string message, uint16_t time) : t
                                                                                message(std::move(message)) {}
 
 std::vector<char> Message::toCharArray() const {
-    uint32_t packageLength = sizeof(uint16_t) +
+    uint32_t packageLength = sizeof(uint32_t) + sizeof(uint16_t) +
             sizeof(uint32_t) + message.size() +
             sizeof(uint32_t) + senderName.size();
 
@@ -207,9 +202,18 @@ std::vector<char> Message::toCharArray() const {
 
     char * dataPointer = buffer;
 
-    uint16_t netTime = htons(time);
-    memcpy(dataPointer, (char *) &netTime, sizeof(uint16_t));
-    dataPointer += sizeof(uint16_t);
+    {
+        uint32_t bodyLength = packageLength - sizeof(uint32_t);
+        uint32_t netBodyLength = htonl(bodyLength);
+        memcpy(dataPointer, (char *) &netBodyLength, sizeof(uint32_t));
+        dataPointer += sizeof(uint32_t);
+    }
+
+    {
+        uint16_t netTime = htons(time);
+        memcpy(dataPointer, (char *) &netTime, sizeof(uint16_t));
+        dataPointer += sizeof(uint16_t);
+    }
 
     {
         // message
@@ -238,63 +242,57 @@ std::vector<char> Message::toCharArray() const {
 
 ClientConnection::ClientConnection(int sockfd, uint32_t clientId, Server * server) : clientId(clientId),
                                                                                      sockfd(sockfd),
-                                                                                     server(server) {}
+                                                                                     server(server) {
+    int nonBlockResult = fcntl(sockfd, F_SETFL, O_NONBLOCK);
+    assert(nonBlockResult == 0);
+}
 
-void ClientConnection::sendMessage(const Message &message) const {
+void ClientConnection::sendMessage(const Message &message) {
     std::vector<char> bytes = message.toCharArray();
-    uint32_t needSend = bytes.size();
-    uint32_t alreadySent = 0;
-    while (alreadySent != needSend) {
-        int send = write(sockfd, bytes.data() + alreadySent, needSend - alreadySent);
-        if (send <= 0) {
-            throw ClientDisconnectedException();
-        }
-        alreadySent += send;
+    for (char byte : bytes) {
+        sendQueue.push_back(byte);
     }
 }
 
-// Must be called only inside read mutex
+void ClientConnection::addNewMessages() {
+    while (nextSend < server->chatSize()) {
+        sendMessage(server->messageAt(nextSend));
+        ++nextSend;
+    }
+}
+
+void ClientConnection::tryReadMessages() {
+    while (canReadNextMessage()) {
+        if (state == INTRODUCE_MESSAGE) {
+            std::string name = getName();
+            userName = name;
+            state = ROUTINE;
+        } else if (state == ROUTINE) {
+            Message message = readMessage();
+            server->receiveMessage(message);
+        }
+    }
+}
+
+bool ClientConnection::canReadNextMessage() const {
+    if (untreatedInput.size() < sizeof(uint32_t)) {
+        return false;
+    }
+    char buffer[sizeof(uint32_t)];
+    for (uint32_t i = 0; i < sizeof(uint32_t); ++i) {
+        buffer[i] = untreatedInput[i];
+    }
+    uint32_t length;
+    memcpy(&length, buffer, sizeof(uint32_t));
+    return untreatedInput.size() >= sizeof(uint32_t) + ntohl(length);
+}
+
 uint32_t Server::chatSize() {
     return chatHistory.size();
 }
 
-// Must be called only inside read mutex
 const Message& Server::messageAt(uint32_t index) {
     return chatHistory[index];
-}
-
-void Server::messageDealing() {
-    std::shared_lock lock(chatHistoryMutex);
-    uint32_t lastSize = 0;
-    while (true) {
-        while (lastSize == chatSize() && !isFinished) {
-            chatHistoryUpdated.wait(lock);
-        }
-        if (isFinished) {
-            break;
-        }
-        std::shared_lock clientsLock(clientsMutex);
-        for (ClientConnection * client : clients) {
-            client->shouldSendMessage.notify_one();
-        }
-        lastSize = chatSize();
-    }
-}
-
-void Server::notifyClientIsDead(uint32_t clientId) {
-    std::unique_lock lock(clientsMutex);
-    notifyClientIsDeadUnsafe(clientId);
-}
-
-void Server::notifyClientIsDeadUnsafe(uint32_t clientId) {
-    auto deletePosition = std::find_if(clients.begin(), clients.end(), [&](ClientConnection * client) {
-        return client->clientId == clientId;
-    });
-    if (deletePosition != clients.end()) {
-        ClientConnection * pointer = *deletePosition;
-        clients.erase(deletePosition);
-        delete pointer;
-    }
 }
 
 Server Server::buildServer(uint16_t portno, uint32_t clientsN) {
@@ -322,6 +320,8 @@ Server Server::buildServer(uint16_t portno, uint32_t clientsN) {
         throw std::exception();
     }
 
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
     return Server(sockfd);
 }
 
@@ -334,51 +334,102 @@ Server::Server(int sockfd) : sockfd(sockfd) {}
 void safeShutdownSocket(int socketfd) {
     if (shutdown(socketfd, SHUT_WR) == 0) {
         char buffer[256];
-        while (read(socketfd, buffer, 255) > 0);
+        while (read(socketfd, buffer, 255) != 0);
     }
 }
 
 void Server::run() {
-    std::thread dealMessage([&] { messageDealing(); });
-    sockaddr_in cli_addr {};
-    uint32_t clilen = sizeof(cli_addr);
-    uint32_t clientId = 0;
+    while (!isFinished) {
+        size_t pollsSize = clients.size() + 1;
+        pollfd polls[pollsSize];
+        polls[0] = pollfd { sockfd, POLLIN, 0 };
 
-    while (true) {
-        int newsockfd = accept(sockfd, (sockaddr *) &cli_addr, &clilen);
+        uint32_t currentIndex = 0;
+        for (ClientConnection * client : clients) {
+            short mask = POLLIN;
+            if (client->haveToSendMessage()) {
+                mask |= POLLOUT;
+            }
+            polls[++currentIndex] = pollfd { client->sockfd, mask, 0 };
+        }
 
-        if (newsockfd < 0) {
-            if (isFinished) {
-                break;
-            } else {
-                std::cerr << "Something gone wrong while accept" << std::endl;
-                continue;
+        int pollResult = poll(polls, pollsSize, -1);
+        if (pollResult < 0) {
+            std::cerr << "Poll returned negative value.\n";
+            throw 1;
+        }
+
+        if (polls[0].revents & POLLIN) {
+            accept();
+        }
+
+        std::vector<uint32_t> disconnectedClients;
+
+        for (uint32_t pollId = 1; pollId < pollsSize; ++pollId) {
+            ClientConnection * client = clients[pollId - 1];
+            short revents = polls[pollId].revents;
+
+            try {
+                if (revents & POLLIN) {
+                    client->readSomething();
+                }
+                if (revents & POLLOUT) {
+                    client->writeSomething();
+                }
+            } catch (ClientDisconnectedException& e) {
+                disconnectedClients.push_back(pollId - 1);
             }
         }
 
-        std::unique_lock lock(clientsMutex);
-        auto client = new ClientConnection(newsockfd, ++clientId, this);
-        if (!isFinished) {
-            clients.push_back(client);
-            threads.push_back(new std::thread(&ClientConnection::run, client));
-        } else {
-            safeShutdownSocket(newsockfd);
-            delete client;
-            break;
+        for (ClientConnection * client: clients) {
+            client->tryReadMessages();
         }
+
+        for (ClientConnection * client: clients) {
+            client->addNewMessages();
+        }
+
+        deleteDisconnectedClients(disconnectedClients);
     }
-    dealMessage.join();
-    for (std::thread * thread : threads) {
-        thread->join();
-        delete thread;
+
+    for (ClientConnection * client : clients) {
+        safeShutdownSocket(client->sockfd);
+        delete client;
     }
 }
 
+void Server::deleteDisconnectedClients(std::vector<uint32_t> disconnectedClients) {
+    std::reverse(disconnectedClients.begin(), disconnectedClients.end());
+
+    for (uint32_t disconnectedClientIndex : disconnectedClients) {
+        ClientConnection * deletedClient = clients[disconnectedClientIndex];
+        std::swap(clients[disconnectedClientIndex], clients.back());
+        clients.pop_back();
+
+        delete deletedClient;
+    }
+}
+
+void Server::accept() {
+    static sockaddr_in cli_addr {};
+    static uint32_t clilen = sizeof(cli_addr);
+    static uint32_t clientId = 0;
+
+    int newsockfd = ::accept(sockfd, (sockaddr *) &cli_addr, &clilen);
+
+    if (newsockfd < 0) {
+        std::cerr << "Something gone wrong while accept" << std::endl;
+        return;
+    }
+
+    auto client = new ClientConnection(newsockfd, ++clientId, this);
+    clients.push_back(client);
+    client->run();
+}
+
 void Server::shutdown() {
-    std::unique_lock lock(clientsMutex);
     ::shutdown(sockfd, SHUT_RDWR);
     isFinished = true;
-    chatHistoryUpdated.notify_one();
     std::vector<ClientConnection *> clientsCopy = clients;
     for (ClientConnection * client : clientsCopy) {
         client->shutdown();
@@ -392,13 +443,9 @@ uint16_t getCurrentTime() {
 }
 
 void Server::receiveMessage(const Message &message) {
-    {
-        std::unique_lock lock(chatHistoryMutex);
-        Message newMessage = message;
-        newMessage.setTime(getCurrentTime());
-        chatHistory.push_back(newMessage);
-    }
-    chatHistoryUpdated.notify_one();
+    Message newMessage = message;
+    newMessage.setTime(getCurrentTime());
+    chatHistory.push_back(newMessage);
 }
 
 int main(int argc, char *argv[]) {
@@ -415,12 +462,13 @@ int main(int argc, char *argv[]) {
     std::thread serverThread([&] { server.run(); });
     std::string s;
     while (getline(std::cin, s)) {
-        if (s == "exit") {
-            server.shutdown();
-            serverThread.join();
+        if (s == "/shutdown") {
             break;
         }
     }
+
+    server.shutdown();
+    serverThread.join();
 
     return 0;
 }
