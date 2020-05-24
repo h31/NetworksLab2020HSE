967522e05f0e0b701e8c7480cbd7055875afca5b
Igor Smirnov <piter3343@gmail.com>
2020-05-24 16:10:48 +0300

    added connection stub

---
+++ individual/vnc/server/src/main/java/ru/spbau/team/vnc/Connection.java
@@ -0,0 +1,15 @@
+package ru.spbau.team.vnc;
+
+import java.net.Socket;
+
+public class Connection {
+
+    private Socket socket;
+
+    public Connection(Socket socket) {
+        this.socket = socket;
+    }
+
+    
+
+}
--- individual/vnc/server/src/main/java/ru/spbau/team/vnc/Server.java
+++ individual/vnc/server/src/main/java/ru/spbau/team/vnc/Server.java
@@ -1,7 +1,32 @@
 package ru.spbau.team.vnc;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private final int port;
+    private final ServerSocket socket;
+
+    public Server(int port) throws IOException {
+        this.port = port;
+        socket = new ServerSocket(port);
+    }
+
+    public void start() {
+        while (true) {
+            try {
+                Socket newConnection = socket.accept();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
