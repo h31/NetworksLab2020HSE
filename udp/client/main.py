79a46d8ae49aeb63d6b92c2dce01a209dcd48eaf
Michael <michael.nik999@gmail.com>
2020-06-16 00:43:18 +0300

    finish client

--- udp/client/main.py
+++ udp/client/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import socket
 
 sys.path.append('../')
@@ -7,57 +8,84 @@ from tftp import *
 
 class tftpClient:
     MAX_SIZE = 4098
+    DEFAULT_PORT = 69
 
-    def __init__(self, hostname, port, timeout):
-        self.addr = hostname, port
+    def __init__(self, hostname, timeout):
+        self.host = hostname
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.settimeout(timeout)
 
-    def read_file(self, file_name, src_file, mode="octet"):
-        self.socket.sendto(ReadRequest(file_name, mode).serialize(), self.addr)
+    def get(self, file_name, src_file, mode):
+        self.socket.sendto(ReadRequest(file_name, mode).serialize(), (self.host, self.DEFAULT_PORT))
 
         cur_block_num = 1
         while True:
-            raw_ans = self.socket.recv(self.MAX_SIZE)
+            raw_ans, addr = self.socket.recvfrom(self.MAX_SIZE)
             ans = deserialize_msg(raw_ans)
-            if ans is not Data or ans.block_num != cur_block_num:
+            # print(ans.block_num, cur_block_num)
+            if type(ans) is not Data or ans.block_num != cur_block_num:
                 raise Exception("unexpected answer", ans)
 
-            self.socket.sendto(Acknowledgment(cur_block_num).serialize(), self.addr)
-            src_file.write(ans.data)
+            self.socket.sendto(Acknowledgment(cur_block_num).serialize(), addr)
+            if mode == Request.OCTET_MODE:
+                src_file.write(ans.data)
+            else:
+                decoded_str = ans.data.decode("ascii")
+                decoded_str = decoded_str.replace("\r\n", "\n")
+                src_file.write(decoded_str)
 
             if len(ans.data) < 512:
                 return
             cur_block_num += 1
 
-    def write_file(self, file_name, src_file, mode="octet",):
-        self.socket.sendto(WriteRequest(file_name, mode).serialize(), self.addr)
+    def put(self, file_name, src_file, mode):
+        self.socket.sendto(WriteRequest(file_name, mode).serialize(), (self.host, self.DEFAULT_PORT))
 
-        raw_ans = self.socket.recv(self.MAX_SIZE)
+        raw_ans, addr = self.socket.recvfrom(self.MAX_SIZE)
         ans = deserialize_msg(raw_ans)
-        if ans is not Acknowledgment or ans.block_num != 0:
+        if type(ans) is not Acknowledgment or ans.block_num != 0:
             raise Exception("unexpected answer", ans)
-
         cur_block_num = 1
         while True:
             data = src_file.read(512)
-            self.socket.sendto(Data(cur_block_num, data).serialize(), self.addr)
+            data = data.encode("ascii")
+            print(len(data))
+            print(Data(cur_block_num, data).serialize())
+            self.socket.sendto(Data(cur_block_num, data).serialize(), addr)
 
-            raw_ans = self.socket.recv(self.MAX_SIZE)
+            raw_ans, addr = self.socket.recvfrom(self.MAX_SIZE)
             ans = deserialize_msg(raw_ans)
-            if ans is not Acknowledgment or ans.block_num != cur_block_num:
-                raise Exception("unexpected answer", ans)
+            if type(ans) is not Acknowledgment or ans.block_num != cur_block_num:
+                raise Exception("unexpected answer at block #%s" % cur_block_num, ans)
 
             if len(data) < 512:
                 return
+            cur_block_num += 1
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("missing host or port")
+    if len(sys.argv) != 6:
+        print("usage: python main.py host [GET|PUT] [octet|netascii] remote_file local_file")
         exit(1)
 
     host = sys.argv[1]
-    port = int(sys.argv[2])
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    address = (host, port)
+    command = sys.argv[2]
+    mode = sys.argv[3]
+    remote_file = sys.argv[4]
+    local_file = sys.argv[5]
+
+    client = tftpClient(host, 10)
+
+    if mode == Request.OCTET_MODE:
+        bin_opt = "b"
+        encoding = None
+    else:
+        bin_opt = ""
+        encoding = "ascii"
+
+    if command == "GET":
+        with open(local_file, "w" + bin_opt, encoding=encoding) as file:
+            client.get(remote_file, file, mode)
+    elif command == "PUT":
+        with open(local_file, "r" + bin_opt, encoding=encoding) as file:
+            client.put(remote_file, file, mode)
--- udp/tftp.py
+++ udp/tftp.py
@@ -20,7 +20,7 @@ class Request(tftpMassage):
         self.mode = mode
 
     def serialize(self):
-        arr = bytearray([self.opcode])
+        arr = bytearray([0, self.opcode])
         arr.extend(self.filename.encode("ascii"))
         arr.append(0)
         arr.extend(self.mode.encode("ascii"))
@@ -32,10 +32,16 @@ class ReadRequest(Request):
     def __init__(self, filename, mode="octet"):
         super().__init__(1, filename, mode)
 
+    def __repr__(self):
+        return 'ReadRequest(%s, %s)' % (self.filename, self.mode)
+
 
 class WriteRequest(Request):
     def __init__(self, filename, mode="octet"):
         super().__init__(2, filename, mode)
+
+    def __repr__(self):
+        return 'WriteRequest(%s, %s)' % (self.filename, self.mode)
 
 
 class Data(tftpMassage):
@@ -44,8 +50,11 @@ class Data(tftpMassage):
         self.block_num = block_num
         self.data = data
 
+    def __repr__(self):
+        return 'Data(%s, %s)' % (self.block_num, self.data)
+
     def serialize(self):
-        arr = bytearray([self.opcode, self.block_num])
+        arr = bytearray([0, self.opcode, 0, self.block_num])
         arr.extend(self.data)
         return arr
 
@@ -55,8 +64,11 @@ class Acknowledgment(tftpMassage):
         super().__init__(4)
         self.block_num = block_num
 
+    def __repr__(self):
+        return 'Acknowledgment(%s)' % self.block_num
+
     def serialize(self):
-        arr = bytearray([self.opcode, self.block_num])
+        arr = bytearray([0, self.opcode, 0, self.block_num])
         return arr
 
 
@@ -66,44 +78,39 @@ class Error(tftpMassage):
         self.err_msg = err_msg
         self.err_num = err_num
 
+    def __repr__(self):
+        return 'Error(%s, %s)' % (self.err_num, self.err_msg)
+
     def serialize(self):
-        arr = bytearray([self.opcode, self.err_num])
+        arr = bytearray([0, self.opcode, 0, self.err_num])
         arr.extend(self.err_msg.encode("ascii"))
         arr.append(0)
         return arr
 
 
 def deserialize_msg(msg_bytes):
-    opcode = int(msg_bytes[:2])
+    opcode = int.from_bytes(msg_bytes[:2], byteorder="big")
+    # print(opcode)
     if opcode == 1 or opcode == 2:
         strings = msg_bytes[2:].split(b'\0', 2)
         file_name = strings[0].decode("ascii")
         mode = strings[1].decode("ascii")
+        if mode != Request.NETASCII_MODE or mode != Request.OCTET_MODE:
+            raise Exception("unknown request mode", mode)
         if opcode == 1:
             return ReadRequest(file_name, mode)
         else:
             return WriteRequest(file_name, mode)
     elif opcode == 3:
-        block_num = int(msg_bytes[2:4])
+        block_num = int.from_bytes(msg_bytes[2:4], byteorder="big")
         data = msg_bytes[4:]
         return Data(block_num, data)
     elif opcode == 4:
-        block_num = int(msg_bytes[2:4])
+        block_num = int.from_bytes(msg_bytes[2:4], byteorder="big")
         return Acknowledgment(block_num)
     elif opcode == 5:
-        err_num = int(msg_bytes[2:4])
-        err_msg = msg_bytes[4:].split('\0')[0]
+        err_num = int.from_bytes(msg_bytes[2:4], byteorder="big")
+        err_msg = msg_bytes[4:].split(b'\0')[0]
+        return Error(err_num, err_msg)
     else:
-        return None
-
-#
-# def recv_msg(sct, bufsz, repeat):
-#     for _ in range(repeat):
-#         try:
-#             raw_ans = sct.recv(bufsz)
-#         except socket.timeout:
-#             continue
-#
-#         return deserialize_msg(raw_ans)
-#
-#     return None
+        raise Exception("unknown opcode", opcode)
